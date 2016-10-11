895655df05def82a5c2c0f98363533db29ec8472
Rupam <roopamroy@gmail.com>
2016-10-10 23:34:24 -0400

    Full screen Work Done

--- src/client/js/HomeController.js
+++ src/client/js/HomeController.js
@@ -2,9 +2,9 @@
     angular.module('analyzerApp')
         .controller('homeController', HomeController);
 
-    HomeController.$inject = ['$scope'];
+    HomeController.$inject = ['$scope', 'exampleFactory'];
 
-    function HomeController($scope) {
+    function HomeController($scope, exampleFactory) {
         $scope.runExpression = runExpression;
         $scope.validateJson = validateJson;
         $scope.beautify = beautify;
@@ -13,6 +13,7 @@
         $scope.jsonText = '[{"id":1,"name":"Leanne Graham","username":"Bret","email":"Sincere@april.biz","address":{"street":"Kulas Light","suite":"Apt. 556","city":"Gwenborough","zipcode":"92998-3874","geo":{"lat":"-37.3159","lng":"81.1496"}},"phone":"1-770-736-8031 x56442","website":"hildegard.org","company":{"name":"Romaguera-Crona","catchPhrase":"Multi-layered client-server neural-net","bs":"harness real-time e-markets"}},{"id":2,"name":"Ervin Howell","username":"Antonette","email":"Shanna@melissa.tv","address":{"street":"Victor Plains","suite":"Suite 879","city":"Wisokyburgh","zipcode":"90566-7771","geo":{"lat":"-43.9509","lng":"-34.4618"}},"phone":"010-692-6593 x09125","website":"anastasia.net","company":{"name":"Deckow-Crist","catchPhrase":"Proactive didactic contingency","bs":"synergize scalable supply-chains"}}]';
         $scope.outputObj = {};
         $scope.error = void 0;
+        $scope.examples = exampleFactory.getExamples();
 
         function runExpression() {
             var jsonObj = {};
@@ -62,8 +63,8 @@
             $scope.error = void 0;
         }
 
-        function toggleFull(isFull){
-            if(isFull){
+        function toggleFull(isFull) {
+            if (isFull) {
                 $scope.fullScreen = void 0;
             }
             else {
---
+++ src/client/js/services/examples.js
@@ -0,0 +1,25 @@
+(function(){
+    angular.module('analyzerApp')
+        .factory('exampleFactory', ExampleFactory);
+
+
+    function ExampleFactory(){
+        return {
+            getExamples: getExamples
+        };
+
+        function getExamples(){
+            return [
+                {
+                    name: "Map",
+                    value: "_.map(jsonObj,function(o){\n\treturn { name: o.name };\n});"
+                },
+                {
+                    name: "Find",
+                    value: "_.find(jsonObj,function(o){\n\treturn o.username === 'Antonette';\n});"
+                }
+            ];
+        }
+    }
+
+})();
